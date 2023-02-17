5c1fb16ef98ffd4bdfb460b57e16ded553f56e57
SammuelPG <palazongarciasamuel@gmail.com>
2023-02-17 09:34:11 +0100

    Creo El proyecto

---
+++ src/refactorizar/clase1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package refactorizar;
+
+/**
+ *
+ * @author dam
+ */
+public class clase1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Este trabajo le ha realizado:");
+        System.out.println("Samuel Palazon Garcia");
+    }
+    
+}
